2f26278cc73b584ef56e3a101fc557a44faeb8d2
Amanda Trinity <amandatrinity16@gmail.com>
2025-10-11 13:51:54 -0500

    feature: implementa cards da loja na página principal

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import GlossFrancine from "./assets/gloss_francine.jpeg"
+import GlossMariMaria from "./assets/gloss_mari_maria.png"
+import GlossMelu from "./assets/gloss_melu.jpeg"
+import GlossKiko from "./assets/gloss_kiko.jpg"
+import Gloss from "./components/gloss";
 
+//componente da nossa tela principal
 function App() {
+  const GlossList = [
+    {imageUrl: GlossFrancine, price:"80",name:"Gloss Francisca"},
+    {imageUrl: GlossMariMaria, price:"100",name:"Gloss Mari Maria"},
+    {imageUrl: GlossMelu, price:"15",name:"Gloss Melu"},
+    {imageUrl: GlossKiko, price:"70",name:"Gloss Kiko"}
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {GlossList.map((gloss) => (
+        <Gloss
+          imageUrl={gloss.imageUrl}
+          price={gloss.price}
+          name={gloss.name}
+        />
+        ))}
     </div>
   );
 }
